a7fd7133669a87a18d1c903acd88f2dc776b9f3a
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-05-24 08:58:41 -0400

    client: game piece positioning

--- client/src/Gamepiece.js
+++ client/src/Gamepiece.js
@@ -1,5 +1,6 @@
 export default function Gamepiece({ value, x, y }) {
-    return (
-        <div className={`piece p${value & 0xF}${(value >> 4) & 0xF}`}></div>
-    );
+  return (
+    <div className={`piece p${value & 0xF}${(value >> 4) & 0xF}`}
+      style={{ top: `${x}00%`, left: `-${y}00%` }}></div>
+  );
 };
