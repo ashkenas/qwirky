a8e763908314033e37d80e89070a477f0a345e74
Jacob Ashkenas <jacobashkenas@gmail.com>
2023-05-24 09:30:31 -0400

    client: piece rendering direction fixes

--- client/src/Gamepiece.js
+++ client/src/Gamepiece.js
@@ -1,6 +1,6 @@
 export default function Gamepiece({ value, x, y }) {
   return (
     <div className={`piece p${value & 0xF}${(value >> 4) & 0xF}`}
-      style={{ top: `${x}00%`, left: `-${y}00%` }}></div>
+      style={{ top: `${-y}00%`, left: `${x}00%` }}></div>
   );
 };
